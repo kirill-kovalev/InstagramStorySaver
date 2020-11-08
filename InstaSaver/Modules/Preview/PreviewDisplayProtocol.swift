09bedd6d2d7ec70a45ebfcafe2042c6e81052e07
Kovalev Kirill <kirilkovalev@ya.ru>
2020-11-09 01:27:20 +0300

    preview Protocol

---
+++ InstaSaver/Modules/Preview/PreviewDisplayProtocol.swift
@@ -0,0 +1,25 @@
+//
+//  PreviewDisplayProtocol.swift
+//  InstaSaver
+//
+//  Created by Кирилл on 04.11.2020.
+//
+
+import UIKit
+
+
+protocol PreviewDisplayDelegate:UIViewController{
+	func displayPreview(_ items:[ISPreviewable], from frame:CGRect, focus on:Int)
+}
+extension PreviewDisplayDelegate{
+	func displayPreview(_ items:[ISPreviewable],from frame:CGRect = .zero, focus on:Int = 0){
+		let vc = PreviewController()
+		vc.content = items
+//		if let navigationController = self.navigationController {
+//			navigationController.pushViewController(vc, animated: true)
+//		}else {
+			self.present(vc, animated: true) { vc.collectionView.scrollToItem(at: IndexPath(item: on, section: 0), at: .centeredHorizontally, animated: false)}
+//		}
+		
+	}
+}
