4f6b4509f2fa8c70395c77b40fe9565a1e01639b
Kovalev Kirill <kirilkovalev@ya.ru>
2020-11-09 13:05:45 +0300

    custom preview collectionView

---
+++ InstaSaver/Modules/Preview/PreviewCollectionView.swift
@@ -0,0 +1,40 @@
+//
+//  PreviewCollectionView.swift
+//  InstaSaver
+//
+//  Created by Кирилл on 09.11.2020.
+//
+
+import UIKit
+
+class PreviewCollectionView: UICollectionView {
+	let backButton: UIButton = {
+		let btn = UIButton(frame: .zero)
+		btn.setImage(Asset.Icons.arrowBack.image.withRenderingMode(.alwaysTemplate), for: .normal)
+		btn.imageView?.tintColor = Asset.Colors.purple.color
+		return btn
+	}()
+		
+	required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+	init() {
+		let layout = UICollectionViewFlowLayout()
+		super.init(frame: .zero, collectionViewLayout: layout)
+		layout.scrollDirection = .horizontal
+		layout.minimumLineSpacing = 0
+		self.isPagingEnabled = true
+		addViews()
+		setupConstraints()
+	}
+	
+	private func addViews() {
+		self.addSubview(backButton)
+	}
+	private func setupConstraints() {
+		backButton.snp.makeConstraints { (make) in
+			make.height.width.equalTo(50)
+			make.left.equalToSuperview().offset(30)
+			make.top.equalTo(safeAreaLayoutGuide).offset(20)
+		}
+	}
+	
+}
