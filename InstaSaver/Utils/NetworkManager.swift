9e18bfecb68347b16f7bb9b220fef8fa6df9e5ba
Kovalev Kirill <kirilkovalev@ya.ru>
2020-11-13 19:21:37 +0300

    caching fixed

--- InstaSaver/Utils/NetworkManager.swift
+++ InstaSaver/Utils/NetworkManager.swift
@@ -29,31 +29,39 @@ class ISNetwork {
 		}
 	}
 	static func download(_ media: ISMedia.Content) -> Observable<URL> {
-		let publisher = PublishSubject<URL>()
+		let publisher = ReplaySubject<URL>.createUnbounded()
 		do {
 			let documentsUrl = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
 			let remoteLink = media.link
 			
 			if let cachedData = cache.object(forKey: remoteLink.lastPathComponent as NSString)?.data,
 			   let stringUrl = String(data: cachedData, encoding: .utf8),
-			   let localUrl = URL(string: stringUrl),
-			   let _ = try? Data(contentsOf: localUrl)
+			   let localUrl = documentsUrl.appendingPathComponent(stringUrl) as? URL,
+			   print("caching",localUrl) == Void()
 			{
+				print("caching found ",localUrl)
 				publisher.on(.next(localUrl))
 				publisher.on(.completed)
 			} else {
 				URLSession.shared.downloadTask(with: remoteLink) { (tempURL, _, error) in
-					if let error = error { publisher.on(.error(error)) }
+					if let error = error {
+						publisher.on(.error(error))
+						return
+					}
+					print("caching temp",tempURL)
 					
 					if let tempURL = tempURL {
 						let destURL = documentsUrl.appendingPathComponent(remoteLink.lastPathComponent)
 						do {
+							print("caching dest",destURL)
+							
 							_ = try? FileManager.default.removeItem(at: destURL)
 							try FileManager.default.copyItem(at: tempURL, to: destURL)
 							
-							if let data = destURL.absoluteString.data(using: .utf8) {
+							if let data = destURL.lastPathComponent.data(using: .utf8) {
 								cache.setObject(DataContainer(data), forKey: remoteLink.lastPathComponent as NSString)
 							}
+							sleep(1)
 							publisher.on(.next(destURL))
 						} catch { publisher.on(.error(error)) }
 					}
